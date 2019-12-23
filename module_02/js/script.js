ebad2394c672f6e54ddc56eedbf74f0334eaecff
YK911 <55898053+YK911@users.noreply.github.com>
2019-12-24 01:12:25 +0200

    add description

--- module_02/js/script.js
+++ module_02/js/script.js
@@ -2,7 +2,6 @@
 // Please, uncomment the line of task to start script!
 //By default, task1.js is running;
 
-
 import array from "./tasks/task_01.js";
 // import calculateEngravingPrice from "./tasks/task_02.js";
 // import findLongestWord from "./tasks/task_03.js";
