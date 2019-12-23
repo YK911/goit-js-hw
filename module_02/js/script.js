57cc3903d3906dcfbd68cf6f6e29460fce2b4dec
YK911 <55898053+YK911@users.noreply.github.com>
2019-12-24 01:05:08 +0200

    remove folder js

--- module_02/js/script.js
+++ module_02/js/script.js
@@ -1,11 +1,12 @@
+"use strict";
 // Please, uncomment the line of task to start script!
 //By default, task1.js is running;
 
 
-// import array from "./tasks/task_01.js";
+import array from "./tasks/task_01.js";
 // import calculateEngravingPrice from "./tasks/task_02.js";
 // import findLongestWord from "./tasks/task_03.js";
 // import formatString from "./tasks/task_04.js";
 // import checkForSpam from "./tasks/task_05.js";
-import calculateTotalPrice from "./tasks/task_06.js";
+// import calculateTotalPrice from "./tasks/task_06.js";
 // import calculateTotalPrice from "./tasks/task_07.js";
