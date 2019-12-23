09a3ffabab0a71feb4837b36ceb80230c1264773
YK911 <55898053+YK911@users.noreply.github.com>
2019-12-24 01:15:37 +0200

    add description

--- module_04/js/script.js
+++ module_04/js/script.js
@@ -1,2 +1,6 @@
+"use strict";
+// Please, uncomment the line of task to start script!
+//By default, task1.js is running;
+
 import account from "./tasks/task_01.js";
 // import inventory from "./tasks/task_02.js";
