48b6a9dfbd33a6ddeb97d64f960b4b35da58da23
YK911 <55898053+YK911@users.noreply.github.com>
2019-12-24 01:17:00 +0200

    add description

--- module_03/js/script.js
+++ module_03/js/script.js
@@ -1,7 +1,11 @@
-// import user from "./tasks/task_01.js";
+"use strict";
+// Please, uncomment the line of task to start script!
+//By default, task1.js is running;
+
+import user from "./tasks/task_01.js";
 // import countProps from "./tasks/task_02.js";
 // import findBestEmployee from "./tasks/task_03.js";
 // import countTotalSalary from "./tasks/task_04.js";
 // import countTotalSalary from "./tasks/task_05.js";
 // import calculateTotalPrice from "./tasks/task_06.js";
-import account from "./tasks/task_07.js";
+// import account from "./tasks/task_07.js";
