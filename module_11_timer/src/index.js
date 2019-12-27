e8829835d7845ce1d642d2a919f1855298fb7f5f
YK911 <55898053+YK911@users.noreply.github.com>
2019-12-27 16:25:26 +0200

    hw_11 timer

---
+++ module_11_timer/src/index.js
@@ -0,0 +1,16 @@
+const clock = $('.clock').FlipClock(3600 * 24 * 3, {
+  clockFace: 'DailyCounter',
+  countdown: true,
+  autoStart: false,
+  callbacks: {
+    stop: function() {
+      $('.message').html('Время прошло');
+    },
+  },
+});
+
+const date = new Date('Dec 31, 2019  24:00:00');
+const currentTime = new Date();
+const time = (date - currentTime) / 1000;
+clock.setTime(time);
+clock.start();
